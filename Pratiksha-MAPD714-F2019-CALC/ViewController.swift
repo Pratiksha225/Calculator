9884901fbdec80b860404a2be1e671c28d3ada70
Pratiksha Kathiriya <pratikshakathirya22597@gmail.com>
2019-09-21 14:12:35 -0400

    Initial Commit

---
+++ Pratiksha-MAPD714-F2019-CALC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pratiksha-MAPD714-F2019-CALC
+//
+//  Created by Pratiksha Kathiriya on 2019-09-21.
+//  Copyright © 2019 centennialcollege. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
